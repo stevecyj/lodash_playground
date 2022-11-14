e9ecaf70fbcd7c8cd0757a1624d98d89ba6f5b47
stevecyj <ycsteven.tsao@gmail.com>
2022-11-14 17:52:33 +0800

    init:chunk,compact

---
+++ src/compact.js
@@ -0,0 +1,5 @@
+let _ = require('lodash')
+
+let arr=[0,1,false,2,'',3,null,undefined,NaN]
+
+console.log(_.compact(arr));
---
+++ src/index.js
@@ -0,0 +1,5 @@
+let _ = require('lodash')
+
+// chunk
+let arr = ['a', 'b', 'c', 'd', 'e', 'f']
+console.log(_.chunk(arr,4));
